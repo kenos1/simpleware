64f75d620793d507882e32a8276fbe909d11c5a1
Kenos <75338427+kenos1@users.noreply.github.com>
2024-12-14 15:26:47 -0700

    attempt to add module documentation

--- mod.ts
+++ mod.ts
@@ -1,5 +1,37 @@
 /**
- * This module contains the router.
+  * This module contains the router.
+  *
+  *
+  * # Simpleware
+  * 
+  * A simple middleware router for use in Deno.
+  * 
+  * ## Example
+  *
+  * A simple hello world example with a:
+  * - Not found handler
+  * - A root handler
+  * - A handler with parameters
+  * - A logger middleware
+  *
+  * @example ```ts
+  * import { Router } from "jsr:@kenos/simpleware";
+  * 
+  * const router = new Router(() => {
+  *   return new Response(null, { status: 404 });
+  * })
+  *   .get("/", () => {
+  *     return new Response("Hello world!");
+  *   })
+  *   .get("/:name", (_, __, ___, params) => {
+  *     return new Response(`Hello ${params?.pathname.groups["name"]}!`);
+  *   })
+  *   .add((last, request) => {
+  *     console.log(`${last.status} ${request.method} ${request.url}`);
+  *     return last;
+  *   });
+  * Deno.serve(router.handler.bind(router));
+  * ```
  */
 
 import Router from "./router.ts"
